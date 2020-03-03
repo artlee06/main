da90848edcdd061ec8741901eaa30b3bd2e9e32a
Arthur Lee <ykarthurlee@gmail.com>
2020-03-03 22:48:41 +0800

    Modified Person to support Remark field

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -39,7 +39,10 @@ public class PersonCard extends UiPart<Region> {
     @FXML
     private Label email;
     @FXML
+    private Label remark;
+    @FXML
     private FlowPane tags;
+    
 
     public PersonCard(Person person, int displayedIndex) {
         super(FXML);
